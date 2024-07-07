993e9c9511fdc96f8de89403c700801d5387bfd7
Shaun Mak <98698774+sunflowerm@users.noreply.github.com>
2024-07-07 12:54:25 +0800

    Remove EsrganPipeline

--- auto1111sdk/__init__.py
+++ auto1111sdk/__init__.py
@@ -1,6 +1,8 @@
 from .pipelines.StableDiffusionPipeline import StableDiffusionPipeline, civit_download
-from .pipelines.EsrganPipelines import EsrganPipeline, download_realesrgan, RealEsrganPipeline
+# from .pipelines.EsrganPipelines import EsrganPipeline, download_realesrgan, RealEsrganPipeline
 from .pipelines.StableDiffusionXLPipeline import StableDiffusionXLPipeline
 from .pipelines.ControlNetModel import ControlNetModel
 
-__all__ = ["StableDiffusionPipeline", "civit_download", "download_realesrgan", "RealEsrganPipeline", "EsrganPipeline", "StableDiffusionXLPipeline","ControlNetModel"]
+# Remove EsrganPipeline
+# __all__ = ["StableDiffusionPipeline", "civit_download", "download_realesrgan", "RealEsrganPipeline", "EsrganPipeline", "StableDiffusionXLPipeline","ControlNetModel"]
+__all__ = ["StableDiffusionPipeline", "civit_download", "StableDiffusionXLPipeline","ControlNetModel"]
